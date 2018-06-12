ea5986aca312cba4cde9e31424c1b4f2f899b2bf
Megshi Thakur <31463510+megshithakur1@users.noreply.github.com>
2018-06-12 03:51:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.post("/addname", (req, res) => {
     var myData = new User(req.body);
     myData.save()
         .then(item => {
-            res.send("Name saved to database");
+            res.send("Name is saved to database");
         })
         .catch(err => {
             res.status(400).send("Unable to save to database");
